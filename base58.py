81ba6379dd5d497e5a4d08a1cb907d0ff8248eca
Heinz <reichswehr@163.com>
2019-06-16 23:28:16 +0800

    the first commit of base58

---
+++ base58.py
@@ -0,0 +1,61 @@
+__b58chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+__b58base = len(__b58chars)
+
+
+def b58encode(v):
+    """ encode v, which is bytes-like object, to base58.
+    """
+    import codecs
+    long_value = int(codecs.encode(v,"hex"), 16)
+    #long_value = int(v.encode("hex_codec"), 16)
+
+    result = ''
+    while long_value >= __b58base:
+        div, mod = divmod(long_value, __b58base)
+        result = __b58chars[mod] + result
+        long_value = div
+    result = __b58chars[long_value] + result
+
+    # Bitcoin does a little leading-zero-compression:
+    # leading 0-bytes in the input become leading-1s
+    nPad = 0
+    for c in v:
+        if c == '\0':
+            nPad += 1
+        else:
+            break
+
+    return (__b58chars[0] * nPad) + result
+
+
+def b58decode(v):
+    """ decode v into a string of len bytes
+    """
+
+    long_value = 0
+    for (i, c) in enumerate(v[::-1]):
+        long_value += __b58chars.find(c) * (__b58base ** i)
+
+    result = ''
+    while long_value >= 256:
+        div, mod = divmod(long_value, 256)
+        result = chr(mod) + result
+        long_value = div
+    result = chr(long_value) + result
+
+    nPad = 0
+    for c in v:
+        if c == __b58chars[0]:
+            nPad += 1
+        else:
+            break
+
+    result = chr(0) * nPad + result
+    return result
+
+if __name__ == "__main__":
+    import hashlib
+    username = 'heinz'
+    base58.b58encode(username.encode())
+    base58.b58decode('Cn8dVGu')
+    base58.b58encode(hashlib.md5(username.encode()).digest())
